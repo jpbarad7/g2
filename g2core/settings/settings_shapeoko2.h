21304178f6a4517ce42d900ea9a3a5043e226340
Jim Barad <31376134+jpbarad7@users.noreply.github.com>
2023-09-25 14:33:30 -0600

    Update settings_shapeoko2.h
    Testing to see if this is the config file used

--- g2core/settings/settings_shapeoko2.h
+++ g2core/settings/settings_shapeoko2.h
@@ -89,7 +89,7 @@
 #define M1_MOTOR_MAP                AXIS_X_EXTERNAL         // 1ma
 #define M1_STEP_ANGLE               1.8                     // 1sa
 #define M1_TRAVEL_PER_REV           40.00                   // 1tr
-#define M1_MICROSTEPS               8                       // 1mi  1,2,4,8,16,32
+#define M1_MICROSTEPS               32                       // 1mi  1,2,4,8,16,32
 #define M1_POLARITY                 0                       // 1po  0=normal, 1=reversed
 #define M1_POWER_MODE               MOTOR_POWER_MODE        // 1pm  TRUE=low power idle enabled
 #define M1_POWER_LEVEL              0.500
@@ -97,7 +97,7 @@
 #define M2_MOTOR_MAP                AXIS_Y_EXTERNAL
 #define M2_STEP_ANGLE               1.8
 #define M2_TRAVEL_PER_REV           40.00
-#define M2_MICROSTEPS               8
+#define M2_MICROSTEPS               32
 #define M2_POLARITY                 0
 #define M2_POWER_MODE               MOTOR_POWER_MODE
 #define M2_POWER_LEVEL              0.500
